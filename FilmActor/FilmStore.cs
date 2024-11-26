f290b352ec09e9331c6104ce721b6231e43c53dd
Racke77 <racke.author@outlook.com>
2024-11-26 12:37:07 +0100

    now checks for multiple actors

--- FilmActor/FilmStore.cs
+++ FilmActor/FilmStore.cs
@@ -11,11 +11,17 @@ namespace FilmActor
     {
         public SakilaDbContext DbContext;
         public Menu Menu;
+        public int FoundActor;
+        public List<string> Actors;
+        public List<string> ActorsId;
         public FilmStore()
         {
             DbContext = new SakilaDbContext();
             Menu = new Menu();
+            Actors = new List<string>();
+            ActorsId = new List<string>();
         }
+        #region Main Program
         public void Storage()
         {
             while (true)
@@ -27,29 +33,19 @@ namespace FilmActor
                     case 0: //Display all actors
                         Console.Clear();
                         DbContext.DisplayAllActorsQuery(); //update query
-                        DbContext.OpenConnection(); //open
-                        var answer = DbContext.CreateCommand().ExecuteReader();
-                        TwoColumnAnswer(answer);
-                        DbContext.CloseConnection(); //close
+                        OpenCloseExecutePrintDouble();
                         Console.ReadLine();
                         break;
                     case 1: //Find films by actor
                         Console.Clear();
                         SearchForActor(); //update query
-                        DbContext.OpenConnection(); //open
-                        answer = DbContext.CreateCommand().ExecuteReader();
-                        SingleColumnAnswer(answer);
-                        DbContext.CloseConnection(); //close
-                        Menu.StartingMenu();
+                        Menu.StartingMenu(); //return to starting-menu
                         Console.ReadLine();
                         break;
                     case 2: //Display all films
                         Console.Clear();
                         DbContext.DisplayAllFilmTitlesQuery();//update query
-                        DbContext.OpenConnection(); //open
-                        answer = DbContext.CreateCommand().ExecuteReader();
-                        SingleColumnAnswer(answer);
-                        DbContext.CloseConnection(); //close
+                        OpenCloseExecutePrintSingle();
                         Console.ReadLine();
                         break;
                     case 3: //Exit program
@@ -58,6 +54,8 @@ namespace FilmActor
                 }
             }
         }
+        #endregion
+        #region Choose how to search
         public void SearchForActor() //for inputting name and updating dbContext query
         {
             Menu.FindActorByXOptions();
@@ -66,18 +64,67 @@ namespace FilmActor
                 case 0: //full name
                     string firstName = Input.InputName("first");
                     string lastName = Input.InputName("last");
-                    DbContext.FindActorByFullNameQuery(firstName, lastName);
+                    FoundManyActors(firstName, lastName);
                     break;
                 case 1: //first name                    
-                    firstName = Input.InputName("first");
-                    DbContext.FindActorByFirstNameQuery(firstName);
+                    firstName = Input.InputName("first"); //ask for input
+                    FoundManyActors(firstName, "");
                     break;
                 case 2: //last name
                     lastName = Input.InputName("last");
-                    DbContext.FindActorByLastNameQuery(lastName);
+                    FoundManyActors("", lastName);
                     break;
             }
         }
+        #endregion
+        #region Found Many Actors Checker
+        public void FoundManyActors(string firstName, string lastName)
+        {
+            DbContext.FoundManyActorsQuery(firstName, lastName); //update query
+            DbContext.OpenConnection(); //open
+            var answer = DbContext.CreateCommand().ExecuteReader();
+            FoundManyMenuPreparation(answer);
+            DbContext.CloseConnection(); //close
+            if (FoundActor > 1)
+            {
+                Menu.MenuUpdate(Actors);
+                DbContext.DisplayFilmByActorIdQuery(ActorsId[Menu.MenuSelectionNormal()]); //select from menu and update query with selected id
+                OpenCloseExecutePrintSingle();
+            }
+            else //if only a single actor is found
+            {
+                if (lastName == "")
+                {
+                    DbContext.FindActorByFirstNameQuery(firstName); //call for films query-update
+                }
+                else if (firstName == "")
+                {
+                    DbContext.FindActorByLastNameQuery(lastName);
+                }
+                else
+                {
+                    DbContext.FindActorByFullNameQuery(firstName, lastName);
+                }
+                OpenCloseExecutePrintSingle();
+            }
+        }
+        public void FoundManyMenuPreparation(SqlDataReader answer)
+        {
+            FoundActor = 0;
+            if (Actors != null)
+            {
+                Actors.Clear();
+                ActorsId.Clear();
+            }
+            while (answer.Read()) //read each row until it has no more
+            {
+                Actors.Add($"{answer[1]} {answer[2]}"); //create name-list for menu
+                ActorsId.Add($"{answer[0]}"); //create id-list for menu
+                FoundActor++;
+            }
+        }
+        #endregion
+        #region SingleColumnAnswer
         public void SingleColumnAnswer(SqlDataReader answer)
         {
             if (answer.HasRows) //does it exist?
@@ -89,6 +136,16 @@ namespace FilmActor
                 Menu.Printer.PrintFailureToFind();
             }
         }
+        public void OpenCloseExecutePrintSingle()
+        {
+            Console.Clear();
+            DbContext.OpenConnection(); //open
+            var answer = DbContext.CreateCommand().ExecuteReader();
+            SingleColumnAnswer(answer);
+            DbContext.CloseConnection(); //close
+        }
+        #endregion
+        #region DoubleColumnAnswer
         public void TwoColumnAnswer(SqlDataReader answer)
         {
             if (answer.HasRows) //does it exist?
@@ -100,5 +157,15 @@ namespace FilmActor
                 Menu.Printer.PrintFailureToFind();
             }
         }
+        public void OpenCloseExecutePrintDouble()
+        {
+            Console.Clear();
+            DbContext.OpenConnection(); //open
+            var answer = DbContext.CreateCommand().ExecuteReader();
+            TwoColumnAnswer(answer);
+            DbContext.CloseConnection();
+        }
+        #endregion
+
     }
 }
--- FilmActor/SakilaDbContext.cs
+++ FilmActor/SakilaDbContext.cs
@@ -17,6 +17,7 @@ namespace FilmActor
         private string CommandString = "";
 
         #region Queries
+        #region Display films
         public void FindActorByFullNameQuery(string firstName, string lastName)
         {
             string commandString =
@@ -32,7 +33,7 @@ namespace FilmActor
                 "select film.title from film_actor " +
                 "inner join actor on film_actor.actor_id = actor.actor_id " +
                 "inner join film on film_actor.film_id = film.film_id " +
-                $"where first_name like '{firstName}'";
+                $"where first_name like '{firstName}' ";
             CommandString = commandString;
         }
         public void FindActorByLastNameQuery(string lastName)
@@ -44,10 +45,12 @@ namespace FilmActor
                 $"where last_name like '{lastName}'";
             CommandString = commandString;
         }
-        public void DisplayAllActorsQuery()
+        public void DisplayFilmByActorIdQuery(string actorId)
         {
-            string commandString = 
-                "select first_name, last_name from actor";
+            string commandString =
+               "select film.title from film_actor " +
+               "inner join film on film_actor.film_id = film.film_id " +
+               $"where film_actor.actor_id = {actorId}";
             CommandString = commandString;
         }
         public void DisplayAllFilmTitlesQuery()
@@ -57,7 +60,40 @@ namespace FilmActor
             CommandString = commandString;
         }
         #endregion
-
+        #region Display actors
+        public void DisplayAllActorsQuery()
+        {
+            string commandString =
+                "select first_name, last_name from actor";
+            CommandString = commandString;
+        }
+        public void FoundManyActorsQuery(string firstName, string lastName)
+        {
+            if (lastName == "")
+            {
+                string commandString =
+                "select actor_id, first_name, last_name from actor " +
+                $"where first_name like '{firstName}' ";
+                CommandString = commandString;
+            }
+            else if (firstName == "")
+            {
+                string commandString =
+                "select actor_id, first_name, last_name from actor " +
+                $"where last_name like '{lastName}'";
+                CommandString = commandString;
+            }
+            else
+            {
+                string commandString =
+                "select actor_id, first_name, last_name from actor " +
+                $"where first_name like '{firstName}' and last_name like '{lastName}'";
+                CommandString = commandString;
+            }
+        }
+        #endregion
+        #endregion
+        #region Connecting to the database
         public SqlCommand CreateCommand()
         {
             var command = new SqlCommand(CommandString, Connection);
@@ -69,9 +105,8 @@ namespace FilmActor
         }
         public void CloseConnection()
         {
-
             Connection.Close();
         }
-
+        #endregion
     }
 }
